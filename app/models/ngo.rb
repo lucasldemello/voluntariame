066eda20e37bf5eff7199ae1f1e72b0950de6d46
Lucas Luan de Melo <111460189+lucasldemelo@users.noreply.github.com>
2022-11-26 19:21:51 -0300

    linter: rubocop policies

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -1,25 +1,27 @@
+# frozen_string_literal: true
+
 class Ngo < ApplicationRecord
-    has_many :contacts
-    has_many :ngo_activities
-    has_many :activities, through: :ngo_activities
+  has_many :contacts
+  has_many :ngo_activities
+  has_many :activities, through: :ngo_activities
 
-    validates :name, :description, :website, :street, :district, :city, :state, :email, presence: true
-    validates :name, length: { minimum: 5}
-    validates :description, length: { maximum: 500}
-    validates :cnpj, length: {is: 14}, allow_blank: true
+  validates :name, :description, :website, :street, :district, :city, :state, :email, presence: true
+  validates :name, length: { minimum: 5 }
+  validates :description, length: { maximum: 500 }
+  validates :cnpj, length: { is: 14 }, allow_blank: true
 
-    geocoded_by :address
-    after_validation :geocode 
+  geocoded_by :address
+  after_validation :geocode
 
+  before_save :set_country
 
-    before_save :set_country
+  private
 
-    private
-    def address
-        [street, number, district, city, state, country].compact.join(', ')
-    end
+  def address
+    [street, number, district, city, state, country].compact.join(', ')
+  end
 
-    def set_country
-        country = 'Brasil'
-    end
+  def set_country
+    country = 'Brasil'
+  end
 end
