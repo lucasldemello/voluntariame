41820827c118b5e2d889056a199135930ac52467
Lucas Luan de Melo <111460189+lucasldemelo@users.noreply.github.com>
2022-11-26 00:24:20 -0300

    linter: rubocop

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
   subject { create(:contact) }
-  it "is valid with valid attributes" do 
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
@@ -10,9 +12,9 @@ RSpec.describe Contact, type: :model do
     it { should belong_to(:ngo) }
   end
 
-  describe "validations" do
-    it{ is_expected.to validate_presence_of :name }    
-    it{ is_expected.to validate_presence_of :cellphone }    
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :cellphone }
     it { should validate_length_of(:name).is_at_least(5) }
   end
 end
--- spec/models/ngo_spec.rb
+++ spec/models/ngo_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Ngo, type: :model do
   subject { create(:ngo) }
-  it "is valid with valid attributes" do 
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
@@ -10,18 +12,17 @@ RSpec.describe Ngo, type: :model do
     it { should have_many(:contacts) }
   end
 
-  describe "validations" do
-    it{ is_expected.to validate_presence_of :name }    
-    it{ is_expected.to validate_presence_of :description }
-    it{ is_expected.to validate_presence_of :website }
-    it{ is_expected.to validate_presence_of :street }    
-    it{ is_expected.to validate_presence_of :district }
-    it{ is_expected.to validate_presence_of :city }
-    it{ is_expected.to validate_presence_of :state }    
-    it{ is_expected.to validate_presence_of :email }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :website }
+    it { is_expected.to validate_presence_of :street }
+    it { is_expected.to validate_presence_of :district }
+    it { is_expected.to validate_presence_of :city }
+    it { is_expected.to validate_presence_of :state }
+    it { is_expected.to validate_presence_of :email }
     it { should validate_length_of(:name).is_at_least(5) }
     it { should validate_length_of(:description).is_at_most(500) }
     it { should validate_length_of(:cnpj).is_equal_to(14) }
   end
-
 end
