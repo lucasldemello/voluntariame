f84397ddff85d40a28851506f4dd36353ba15ece
Lucas Luan de Melo <111460189+lucasldemelo@users.noreply.github.com>
2022-11-28 00:08:22 -0300

    fix: permit saving activities

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Activity do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :used_count
+  permit_params :name, :used_count
   #
   # or
   #
