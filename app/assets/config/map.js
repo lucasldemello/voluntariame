73e561e438464d42b6acd1b35ccf72b113bfe47c
Lucas Luan de Melo <111460189+lucasldemelo@users.noreply.github.com>
2022-11-28 01:43:29 -0300

    feat: add description to infowindow

--- app/assets/config/map.js
+++ app/assets/config/map.js
@@ -93,41 +93,47 @@ function showNgosMarkers(ngos) {
         var address = ngo["address"];
         var phoneNumber = "99999999";
         var website = ngo["website"];
+        var description = ngo["description"];
         bounds.extend(latlng);
-        createMarker(latlng, name, address, phoneNumber, website, index+1);
+        createMarker(latlng, name, address, phoneNumber, website, description, index+1);
     });
     map.fitBounds(bounds);
 }
 
-function buildHtml(name, address, phoneNumber, website, latLng){
+function buildHtml(name, address, phoneNumber, website, description, latLng){
     var html = `
-        <div class="ngo-info-window"> 
-            <div class="ngo-info-name">
-                ${name}
-            </div>
-            <div class="ngo-info-status">
-                <i class="fas fa-globe"></i> ${website}
-            </div>
-            <div class="ngo-info-address">
-                <div class="circle">
-                    <i class="fas fa-location-arrow"></i> 
-                
-                <a title="Veja a rota"
-                href='https://www.google.com/maps/dir/?api=1&origin=${startPos["latitude"]},${startPos["longitude"]}&destination=${latLng.lat()},${latLng.lng()}' target="_blank">${address}</a>
-                </div>
-                </div> 
-            <div class="ngo-info-phone">
-                <div class="circle">
-                    <i class="fas fa-phone-alt"></i>${phoneNumber}
-                </div>    
-                
-            </div>
+    <div class="store-info-window"> 
+    <div class="store-info-name">
+        ${name}
+    </div>
+    <div class="store-info-status">
+        ${description}
+    </div>
+    <div class="store-info-phone">
+        <div class="circle">
+            <i class="fas fa-globe"></i>
         </div>
+        <a title="Website da ONG" href=${website}>${website}</a>
+    </div>
+    <div class="store-info-address">
+        <div class="circle">
+            <i class="fas fa-location-arrow"></i> 
+        </div>
+        <a title="Rotas"
+        href='https://www.google.com/maps/dir/?api=1&origin=${startPos["latitude"]},${startPos["longitude"]}&destination=${latLng.lat()},${latLng.lng()}' target="_blank">${address}</a>
+    </div> 
+    <div class="store-info-phone">
+        <div class="circle">
+            <i class="fas fa-phone-alt"></i>
+        </div>    
+        ${phoneNumber}
+    </div>
+</div>
     `;
     return html;
 }
 
-function createMarker(latlng, name, address, phoneNumber, website, index){
+function createMarker(latlng, name, address, phoneNumber, website, description, index){
     var marker = new google.maps.Marker({
         map: map,
         position: latlng,
@@ -136,7 +142,7 @@ function createMarker(latlng, name, address, phoneNumber, website, index){
 
     google.maps.event.addListener(marker, 'click', function() { 
         map.panTo(marker.getPosition());
-        infoWindow.setContent(buildHtml(name, address, phoneNumber, website, latlng));
+        infoWindow.setContent(buildHtml(name, address, phoneNumber, website, description, latlng));
         infoWindow.open(map, marker);
     });
     
